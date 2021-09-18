2a8649fca14f8aa26379211397e4eb447bc7890c
Savioli Pri <stobbeglaucya@gmail.com>
2021-09-18 12:00:56 -0300

    Adicionar resolução exercício AreaTrianguloRet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double medidaBase, medidaAltura, medidaArea;
+            Console.Write("Base:");
+            medidaBase = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Altura:");
+            medidaAltura = Convert.ToDouble(Console.ReadLine());
+            Console.ReadLine();
+
+            medidaArea = medidaBase*medidaAltura /2;
+
+            Console.WriteLine($"Área: {medidaArea}");
+            
+        }
+    }
+}
